03c49726f6b1f49970efca68e6c0c10e99dbde4d
Gustavo Seabra <42077000+gmseabra@users.noreply.github.com>
2021-08-20 10:26:15 -0400

    Add files via upload

---
+++ fingerprinting/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package has tools for fingerprinting chemical databases
+
+Author: Gustavo Seabra
+        Department of Medicinal Chemistry
+        College of Pharmacy
+        Univerity of Florida
+        Gainesville, FL, USA
+        seabra - at - cop - dot - ufl - dot - edu[summary]
+"""
